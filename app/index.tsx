44192d561a12155b6e830179fe672187b60d8961
Pavol Biačko <xbiacko@fi.muni.cz>
2025-02-23 22:54:13 +0100

    feat: main page template

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,49 @@
-import { Text, View } from "react-native";
-import "../global.css";
+import { StatusBar } from "expo-status-bar";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+import CustomButton from "@/components/CustomButton";
+import { images } from "../constants";
+
+export default function App() {
   return (
-    <View className="bg-orange-500">
-      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="w-full h-full justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[134px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Discover Endlesss Possibilites with{" "}
+              <Text className="text-secondary-200">
+                Aura
+              </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[120px] h-[10px] absolute -bottom-1 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless exploration with Aora
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => null}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
