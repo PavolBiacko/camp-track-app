1eff471418f8c2b016b04d326422f9a136fe9599
Pavol Biačko <xbiacko@fi.muni.cz>
2025-03-03 17:16:21 +0100

    chore: colors config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "#161622",
-        secondary: {
-          DEFAULT: "#FF9C01",
-          100: "#FF9001",
-          200: "#FF8E01",
-        },
-        black: {
-          DEFAULT: "#000",
-          100: "#1E1E2D",
-          200: "#232533",
-        },
-        gray: {
-          100: "#CDCDE0",
-        },
+        secondary: "#FF9001",
+        black: "#1E1E2D",
+        gray: "#CDCDE0",
+        while: "#FFFFFF",
       },
       fontFamily: {
         pthin: ["Poppins-Thin", "sans-serif"],
