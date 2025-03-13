398fbf2f0ea7df12ceea39ccdf1c895715228af1
Pavol Biačko <xbiacko@fi.muni.cz>
2025-03-13 15:50:19 +0100

    chore: small cosmetics

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,7 +5,7 @@ import { useCapitalizeWord } from '@/hooks/useUtilHooks'
 import { AuthFormProps } from '@/types/form'
 import { Link } from 'expo-router'
 import { useState } from 'react'
-import { Alert, Image, KeyboardAvoidingView, ScrollView, Text, View } from 'react-native'
+import { Alert, Image, ScrollView, Text, View } from 'react-native'
 
 const AuthForm = ({ title, fields, initialValues, onSubmit, linkData }: AuthFormProps) => {
 
@@ -24,41 +24,39 @@ const AuthForm = ({ title, fields, initialValues, onSubmit, linkData }: AuthForm
   };
 
   return (
-    <KeyboardAvoidingView keyboardVerticalOffset={0}>
-      <ScrollView keyboardShouldPersistTaps="handled">
-        <View className="w-full min-h-[90vh] justify-center px-4 my-6">
-          <Image source={images.logowithtext} resizeMode="contain" className="self-center w-[300px] h-[100px]" />
-          <Text className="text-2xl text-white mt-5 font-pbold">{title}</Text>
+    <ScrollView keyboardShouldPersistTaps="handled">
+      <View className="w-full min-h-[90vh] justify-center px-4 my-6">
+        <Image source={images.logowithtext} resizeMode="contain" className="self-center w-[300px] h-[100px]" />
+        <Text className="text-2xl text-white mt-5 font-pbold">{title}</Text>
 
-          {fields.map(({ title, formDataTypeKey, placeholder, otherStyles }) => (
-            <FormField
-              key={title}
-              title={useCapitalizeWord(title)!}
-              formDataTypeKey={formDataTypeKey}
-              value={form[formDataTypeKey]}
-              placeholder={placeholder}
-              handleChangeText={(e: string) => setForm({ ...form, [formDataTypeKey]: e })}
-              otherStyles={otherStyles || "mt-7"}
-            />
-          ))}
-
-          <CustomButton
-            title={title}
-            handlePress={handleSubmit}
-            isPrimary={true}
-            containerStyles="mt-7"
-            isLoading={isSubmitting}
+        {fields.map(({ title, formDataTypeKey, placeholder, otherStyles }) => (
+          <FormField
+            key={title}
+            title={useCapitalizeWord(title)!}
+            formDataTypeKey={formDataTypeKey}
+            value={form[formDataTypeKey]}
+            placeholder={placeholder}
+            handleChangeText={(e: string) => setForm({ ...form, [formDataTypeKey]: e })}
+            otherStyles={otherStyles || "mt-7"}
           />
+        ))}
 
-          <View className="justify-center pt-5 flex-row gap-2">
-            <Text className="text-lg text-gray font-pregular">{linkData.prelinkText}</Text>
-            <Link href={linkData.linkHref} className="text-lg font-psemibold text-secondary">
-              {linkData.linkText}
-            </Link>
-          </View>
+        <CustomButton
+          title={title}
+          handlePress={handleSubmit}
+          isPrimary={true}
+          containerStyles="mt-7"
+          isLoading={isSubmitting}
+        />
+
+        <View className="justify-center pt-5 flex-row gap-2">
+          <Text className="text-lg text-gray font-pregular">{linkData.prelinkText}</Text>
+          <Link href={linkData.linkHref} className="text-lg font-psemibold text-secondary">
+            {linkData.linkText}
+          </Link>
         </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+      </View>
+    </ScrollView>
   )
 }
 
