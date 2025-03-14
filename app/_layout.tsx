ae2a1759b445410a38b7b18c5edfef906d542849
Pavol Biačko <xbiacko@fi.muni.cz>
2025-03-14 15:08:31 +0100

    chore: layout adjusted based on useAuth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import useAuth from '@/hooks/useAuth';
 import { useAppFonts } from '@/hooks/useUtilHooks';
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from 'react';
@@ -8,6 +9,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
 
   const [fontsLoaded, error] = useAppFonts();
+  const { session, isLoading } = useAuth();
 
   useEffect(() => {
     if (error) throw error;
@@ -16,11 +18,12 @@ export default function RootLayout() {
 
   if (!fontsLoaded && !error) return null;
 
+  if (isLoading) return null;
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ animation: 'none' }} />
-      <Stack.Screen name="(auth)" options={{ animation: 'ios_from_right' }} />
-      <Stack.Screen name="(tabs)" options={{ animation: 'ios_from_right' }} />
+      {!session && <Stack.Screen name="(auth)" options={{ animation: 'ios_from_right' }} />}
+      {session && <Stack.Screen name="(tabs)" options={{ animation: 'ios_from_right' }} />}
     </Stack>
   )
 }
