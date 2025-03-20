88cb98b2cc738c27c77059d29b94131da0b9ee52
Pavol Biačko <xbiacko@fi.muni.cz>
2025-03-20 20:56:29 +0100

    chore: logout button moved

--- app/(tabs)/gallery.tsx
+++ app/(tabs)/gallery.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '@/components/CustomButton';
+import authRepository from '@/repositories/authRepository';
+import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Gallery = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submit = async () => {
+    try {
+      setIsLoading(true);
+      await authRepository.logout()
+      router.replace("/welcome");
+    } catch (error: any) {
+      Alert.alert("Pozor!", error.message);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <SafeAreaView>
-      <Text>Gallery</Text>
+      <CustomButton
+        title="Odhlás sa"
+        handlePress={submit}
+        isPrimary={true}
+        iconStyles="w-8 h-8"
+        containerStyles="w-full mt-5"
+        isLoading={isLoading}
+      />
     </SafeAreaView>
   )
 }
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,37 +1,14 @@
-import CustomButton from '@/components/CustomButton';
-import authRepository from '@/repositories/authRepository';
-import { router } from 'expo-router';
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
 
-  const [isLoading, setIsLoading] = useState(false);
 
-  const submit = async () => {
-    try {
-      setIsLoading(true);
-      await authRepository.logout()
-      router.replace("/welcome");
-    } catch (error: any) {
-      Alert.alert("Pozor!", error.message);
-      return;
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   return (
     <SafeAreaView>
-      <CustomButton
-        title="Odhlás sa"
-        handlePress={submit}
-        isPrimary={true}
-        iconStyles="w-8 h-8"
-        containerStyles="w-full mt-5"
-        isLoading={isLoading}
-      />
+      <Text>Home</Text>
     </SafeAreaView>
   )
 }
