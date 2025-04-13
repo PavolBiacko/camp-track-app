83dac60af05b10ec094cf6e1c7e6859c80a9b64f
Pavol Biačko <xbiacko@fi.muni.cz>
2025-04-13 12:47:07 +0200

    fix: money images transparent

--- constants/images.js
+++ constants/images.js
@@ -2,21 +2,21 @@ import logowithtext from "@/assets/images/logo-with-text.png";
 import logo from "@/assets/images/logo.png";
 
 // finance
-import cents1 from "@/assets/images/finance/cents-1.jpeg";
-import cents10 from "@/assets/images/finance/cents-10.jpeg";
-import cents2 from "@/assets/images/finance/cents-2.jpeg";
-import cents20 from "@/assets/images/finance/cents-20.jpeg";
-import cents5 from "@/assets/images/finance/cents-5.jpeg";
-import cents50 from "@/assets/images/finance/cents-50.jpeg";
-import euro1 from "@/assets/images/finance/euro-1.jpeg";
-import euro10 from "@/assets/images/finance/euro-10.jpeg";
-import euro100 from "@/assets/images/finance/euro-100.jpeg";
-import euro2 from "@/assets/images/finance/euro-2.jpeg";
-import euro20 from "@/assets/images/finance/euro-20.jpeg";
-import euro200 from "@/assets/images/finance/euro-200.jpeg";
-import euro5 from "@/assets/images/finance/euro-5.jpeg";
-import euro50 from "@/assets/images/finance/euro-50.jpeg";
-import euro500 from "@/assets/images/finance/euro-500.jpeg";
+import cents1 from "@/assets/images/finance/cents-1.png";
+import cents10 from "@/assets/images/finance/cents-10.png";
+import cents2 from "@/assets/images/finance/cents-2.png";
+import cents20 from "@/assets/images/finance/cents-20.png";
+import cents5 from "@/assets/images/finance/cents-5.png";
+import cents50 from "@/assets/images/finance/cents-50.png";
+import euro1 from "@/assets/images/finance/euro-1.png";
+import euro10 from "@/assets/images/finance/euro-10.png";
+import euro100 from "@/assets/images/finance/euro-100.png";
+import euro2 from "@/assets/images/finance/euro-2.png";
+import euro20 from "@/assets/images/finance/euro-20.png";
+import euro200 from "@/assets/images/finance/euro-200.png";
+import euro5 from "@/assets/images/finance/euro-5.png";
+import euro50 from "@/assets/images/finance/euro-50.png";
+import euro500 from "@/assets/images/finance/euro-500.png";
 
 export default {
   logo,
