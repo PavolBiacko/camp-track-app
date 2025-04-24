6e4ae955af0cd1443a2563f683957f8683f28810
Pavol Biačko <xbiacko@fi.muni.cz>
2025-04-24 16:18:56 +0200

    chore: reorganized finance layout again

--- app/(main)/(tabs)/finance.tsx
+++ app/(main)/(tabs)/finance.tsx
@@ -5,7 +5,6 @@ import { View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Finance = () => {
-  const containerStyles = "rounded-3xl w-[47%]"
   const textStyles = "text-center text-2xl font-pbold"
 
   const { user } = useAuth();
@@ -16,36 +15,45 @@ const Finance = () => {
 
   return (
     <SafeAreaView className='justify-center h-full'>
-      <View className="h-1/4 flex-row justify-between m-5">
+      <View className="h-1/4 flex-row justify-between mx-5 my-2">
         <CustomButton
           title='Celkový prehľad'
           action='background'
           handlePress={() => router.push('/(main)/(finance)/overview')}
-          containerStyles={containerStyles}
+          containerStyles="rounded-3xl w-[48%]"
           textStyles={textStyles}
         />
         <CustomButton
           title='Nastavenia účtov detí'
           action='background'
           handlePress={() => router.push({ pathname: '/(main)/(finance)/accounts', params: { leaderId: user.id } })}
-          containerStyles={containerStyles}
+          containerStyles="rounded-3xl w-[48%]"
           textStyles={textStyles}
         />
       </View>
-      <View className="h-1/3 m-5">
+      <View className="h-[12.5%] mx-5 my-2">
         <CustomButton
-          title='Návšteva bufetu'
-          action='tertiary'
-          handlePress={() => router.push('/(main)/(finance)/buffet')}
+          title='Výpis transakcií'
+          action='background'
+          handlePress={() => router.push('/(main)/(finance)/transactions')}
           containerStyles="rounded-3xl h-full"
           textStyles={textStyles}
         />
       </View>
-      <View className="h-1/6 m-5">
+      <View className="h-[12.5%] mx-5 my-2">
         <CustomButton
           title='Výpočet výdavkov'
           action='secondary'
           handlePress={() => router.push('/(main)/(finance)/calculation')}
+          containerStyles="rounded-3xl h-full"
+          textStyles={textStyles}
+        />
+      </View>
+      <View className="h-1/3 mx-5 my-2">
+        <CustomButton
+          title='Návšteva bufetu'
+          action='tertiary'
+          handlePress={() => router.push('/(main)/(finance)/buffet')}
           containerStyles="rounded-3xl h-full"
           textStyles={textStyles}
         />
