edb152e15366809ce7e4dff1ee2c50b527fc3ac7
Pavol Biačko <xbiacko@fi.muni.cz>
2025-05-01 00:06:46 +0200

    START AGAIN HERE, IF SOMETHING IS MESSED UP

--- components/custom/finance/overview/FinanceOverviewFooter.tsx
+++ components/custom/finance/overview/FinanceOverviewFooter.tsx
@@ -3,15 +3,17 @@ import { View } from 'react-native'
 
 const FinanceOverviewFooter = () => {
   return (
-    <View className="border-t border-outline-300 justify-center items-center py-4">
-      <CustomButton
-        title="Vyplatenie bufetu"
-        action="primary"
-        handlePress={() => { }}
-        textStyles="text-2xl text-center"
-        containerStyles="h-16 rounded-3xl px-5"
-      />
-    </View>
+    <>
+      <View className="border-t border-outline-300 justify-center items-center py-4">
+        <CustomButton
+          title="Vyplatenie bufetu"
+          action="primary"
+          handlePress={() => { }}
+          textStyles="text-2xl text-center"
+          containerStyles="h-16 rounded-3xl px-5"
+        />
+      </View>
+    </>
   )
 }
 
