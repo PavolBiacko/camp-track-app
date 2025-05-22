275399610874d31c9e993e40ce2f477994e94b7a
Pavol Biačko <xbiacko@fi.muni.cz>
2025-05-22 04:16:44 +0200

    feat: showing badge near child

--- components/custom/children/ParentChildAccountLine.tsx
+++ components/custom/children/ParentChildAccountLine.tsx
@@ -1,3 +1,5 @@
+import { Badge, BadgeText } from '@/components/ui/badge';
+import { useGroupById } from '@/hooks/models/useGroups';
 import { ParentChildLineProps } from '@/types/children';
 import { router } from 'expo-router';
 import { Text, TouchableOpacity, View } from 'react-native';
@@ -5,6 +7,8 @@ import { ClassNameValue, twMerge } from 'tailwind-merge';
 
 const ParentChildAccountLine = (props: ParentChildLineProps) => {
   const { id, firstName, lastName, groupId, accountBalance } = props.child;
+
+  const { group, isLoading, isError } = useGroupById(groupId);
 
   const backgroundStyles: ClassNameValue = `bg-background-300 border-2 border-outline-500 ${props.containerStyles}`
   const textStyles: ClassNameValue = `text-2xl ${props.textStyles}`;
@@ -17,12 +21,17 @@ const ParentChildAccountLine = (props: ParentChildLineProps) => {
       className={
         twMerge(
           "flex-row justify-between items-center",
-          "rounded-xl px-6 my-3 w-11/12 h-28",
+          "rounded-xl px-6 my-3 w-11/12 h-32",
           backgroundStyles
         )}>
-      <View className='pt-1 gap-1'>
-        <Text className={twMerge(textStyles, "font-pbold text-typography-800")}>{firstName}</Text>
-        <Text className={twMerge(textStyles, "font-pbold text-typography-800")}>{lastName}</Text>
+      <View className='gap-2'>
+        <Badge className="w-24 justify-center bg-secondary-500 rounded-xl mt-2">
+          <BadgeText className='text-typography-950 text-center font-pbold px-2'>{group?.number ? `${group.number}. odd` : "..."}</BadgeText>
+        </Badge>
+        <View className=''>
+          <Text className={twMerge(textStyles, "font-pbold text-typography-800")}>{firstName}</Text>
+          <Text className={twMerge(textStyles, "font-pbold text-typography-800")}>{lastName}</Text>
+        </View>
       </View>
       <Text className={
         twMerge(
